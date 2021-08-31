80e5361c55087c82a8fe37d43e561ecd444b740e
Dan-code-str <70246536+Dan-code-str@users.noreply.github.com>
2021-08-31 18:55:36 -0500

    Add files via upload
    Clase usuario añadida a example (mover)

---
+++ app/src/main/java/com/example/freelancer_app/Usuario.java
@@ -0,0 +1,85 @@
+public class Usuario{
+    int usuarioId;
+    String nombre;
+    String clave;
+    String correo;
+    String pais;
+    String descripción;
+    //img imagen (?)
+
+
+    public int getUsuarioId() {
+        return this.id;
+    }
+
+    public void setUsuarioId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getClave() {
+        return this.clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getCorreo() {
+        return this.correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPais() {
+        return this.pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getDescripción() {
+        return this.descripción;
+    }
+
+    public void setDescripción(String descripción) {
+        this.descripción = descripción;
+    }
+
+
+
+    public Usuario(int id, String nombre, String clave, String correo, String pais, String descripción) {
+        this.id = id;
+        this.nombre = nombre;
+        this.clave = clave;
+        this.correo = correo;
+        this.pais = pais;
+        this.descripción = descripción;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", nombre='" + getNombre() + "'" +
+            ", clave='" + getClave() + "'" +
+            ", correo='" + getCorreo() + "'" +
+            ", pais='" + getPais() + "'" +
+            ", descripción='" + getDescripción() + "'" +
+            "}";
+    }
+
+    
+}
